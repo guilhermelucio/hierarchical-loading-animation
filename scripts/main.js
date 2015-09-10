0074a8cca3fc18fa308b21be9bc4b415cda4086b
Guilherme Lucio <guilhermelucio.design@gmail.com>
2015-09-10 09:54:57 -0300

    Creating the function runReverseAnimation

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,3 @@
-Object.prototype.eachElement = function (callback) {
-	var elements = this;
-	var counter = 0;
-	
-	(function animate (counter) {
-		setTimeout(function () {
-			callback(element[counter]);
-			counter++;
-
-			if (counter < element.length) {
-				animate(counter);
-			}
-				
-		},60);
-	})(0);
-}
-
 var hierarchicalLoadingAnimation = function () {
 
 	var Animation = this;
@@ -27,22 +10,31 @@ var hierarchicalLoadingAnimation = function () {
 		interval: 500
 	}
 
+	/*
+		Array of elements that contain the class specified at the variable "properties"
+		Theses elements will receive the animation effect
+	*/
 	var elements = [];
 
+	/*
+		Entry point of the Class hierarchicalLoadingAnimation is the function below
+		This function is responsible for getting a list of elements and run the animate function
+		The first thing I want to get is a list of elements where I'm going to apply the effect
+	*/
 	Animation.animate = function () {
+		
 		/*
-			Entry point of the Class hierarchicalLoadingAnimation
-			This function is responsible for getting a list of elements and run the animate function
-			The first thing I want to get is a list of elements where I'm going to apply the effect
+			The function below is responsible for find all elements at the page and saving them
+			in an array variable "elements", a private variable used throughout the script
 		*/
-		var elements = Animation.locateElements();
+		Animation.locateElements();
 
 
 		/*
 			Animation function is just responsible for getting a list of elements and applying 
 			the animation, caring that the interval value will be respect between the animations
 		*/
-		var animation = Animation.runAnimation();
+		Animation.runAnimation();
 	};
 
 	Animation.locateElements = function (data) {
@@ -50,13 +42,6 @@ var hierarchicalLoadingAnimation = function () {
 			Return array with elements, based on a class passed by parameter or a default class box
 		*/
 		element = document.querySelectorAll(properties.selector);
-	};
-
-	Animation.setElementSelector = function (element) {
-		/*
-			Using a method setter to set the value of a private variable selector
-		*/
-		properties.element = element;
 	};
 
 	Animation.getElementSelector = function () {
@@ -82,11 +67,29 @@ var hierarchicalLoadingAnimation = function () {
 	}
 
 	Animation.runAnimation = function () {
+		
+		/*
+			eachElement is a prototype of the root element Object, it's defined at the end of the document
+			it's possible to pass a callback function to the prototype function, which will execute considering
+			the callback function and the properties of the animation, coming from the private variable "properties"
+		*/
 		element.eachElement(function(e){
 			if (typeof e === 'object') 
 				e.classList.add("show");
-		});
+		}, properties);
+	
 	};
+
+	Animation.runReverseAnimation = function () {
+
+		element.eachElement(function(e){
+			if (typeof e === 'object')
+				e.classList.remove("show");
+		},properties);
+
+	}
+	
+	// Calling the main function 
 	Animation.animate();
 
 };
@@ -94,3 +97,20 @@ var hierarchicalLoadingAnimation = function () {
 window.onload = function (e) {
 	var animate = new hierarchicalLoadingAnimation();
 };
+
+Object.prototype.eachElement = function (callback, properties) {
+	var elements = this;
+	var counter = 0;
+	
+	(function animate (counter) {
+		setTimeout(function () {
+			callback(element[counter]);
+			counter++;
+
+			if (counter < element.length) {
+				animate(counter);
+			}
+				
+		},properties.interval);
+	})(0);
+}
